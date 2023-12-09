5bd18e86d3966a99de334ae11e84176dda0f329a
FZuccala <46850877+FZuccala@users.noreply.github.com>
2023-12-09 14:58:52 -0300

    Cleanup and refactor

--- src/components/Molecule/Card/index.tsx
+++ src/components/Molecule/Card/index.tsx
@@ -1,64 +1,61 @@
-"use client";
-import { Card, CardMedia, CardContent, Typography, styled, CardActions, IconButton, } from '@mui/material'
-import React, {useState} from 'react'
-import { TUser } from '@/typings'
+'use client';
+import { Card, CardMedia, CardContent, Typography, styled, CardActions, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { TUser } from '@/typings';
 import LanguageIcon from '@mui/icons-material/Language';
 import EditIcon from '@mui/icons-material/Edit';
 import InfoIcon from '@mui/icons-material/Info';
 import { useUserStore } from '@/store/userStore';
 import { useGlobalStore } from '@/store/globalStore';
+import CustomIconButton from '@/components/Atoms/CustomIconButton';
 
 const StyledCard = styled(Card)({
   maxWidth: 345,
-})
+});
 const StyledCardMedia = styled(CardMedia)({
   height: 250,
-})
-const StyledDescription = styled(Typography)(({viewMore}: {viewMore: boolean}) => ({
+});
+const StyledDescription = styled(Typography)(({ viewMore }: { viewMore: boolean }) => ({
   display: '-webkit-box',
   WebkitLineClamp: viewMore ? 'unset' : 2,
   WebkitBoxOrient: 'vertical',
   overflow: 'hidden',
   textOverflow: 'ellipsis',
-}))
+}));
 const StyledViewMoreAction = styled(Typography)({
   cursor: 'pointer',
   color: 'blue',
   textDecoration: 'underline',
-})
+});
 type Props = {
-  data: TUser
-}
+  data: TUser;
+};
 
-const CustomCard = ({data}: Props) => {
+const CustomCard = ({ data }: Props) => {
   const [viewMore, setViewMore] = useState(false);
-  const {setSelectedUser, getUserById} = useUserStore();
-  const {
-    setModalInformation,
-   } = useGlobalStore();
+  const { setSelectedUser, getUserById } = useUserStore();
+  const { setModalInformation } = useGlobalStore();
   const handleClickGoToWebsite = () => {
-    window.open(data.website, '_blank')
-  }
+    window.open(data.website, '_blank');
+  };
   const handleEditIconClick = () => {
-    setSelectedUser(getUserById(data.id) as TUser)
+    setSelectedUser(getUserById(data.id) as TUser);
     setModalInformation({
       show: true,
-      type: 'edit'
-    })
-  }
+      type: 'edit',
+    });
+  };
   const handleInfoIconClick = () => {
-    setSelectedUser(getUserById(data.id) as TUser)
+    setSelectedUser(getUserById(data.id) as TUser);
     setModalInformation({
       show: true,
-      type: 'info'
-    })
-  }
+      type: 'info',
+    });
+  };
   return (
     <StyledCard>
-      <StyledCardMedia
-        image={data.avatar}
-        title={data.name}/>
-              <CardContent>
+      <StyledCardMedia image={data.avatar} title={data.name} />
+      <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
           {data.name}
         </Typography>
@@ -68,18 +65,12 @@ const CustomCard = ({data}: Props) => {
         <StyledViewMoreAction onClick={() => setViewMore(!viewMore)}>{viewMore ? 'View Less' : 'View More'}</StyledViewMoreAction>
       </CardContent>
       <CardActions>
-        <IconButton aria-label="website" onClick={() => handleClickGoToWebsite()}>
-          <LanguageIcon />
-        </IconButton>
-        <IconButton aria-label="edit" onClick={() => handleEditIconClick()}>
-          <EditIcon />
-        </IconButton>
-        <IconButton aria-label='view all' onClick={() => handleInfoIconClick()}>
-          <InfoIcon />
-        </IconButton>
+        <CustomIconButton ariaLabel="website" icon={<LanguageIcon />} handleClick={() => handleClickGoToWebsite()} />
+        <CustomIconButton ariaLabel="edit" icon={<EditIcon />} handleClick={() => handleEditIconClick()} />
+        <CustomIconButton ariaLabel="view all" icon={<InfoIcon />} handleClick={() => handleInfoIconClick()} />
       </CardActions>
     </StyledCard>
-  )
-}
+  );
+};
 
-export default CustomCard
+export default CustomCard;
